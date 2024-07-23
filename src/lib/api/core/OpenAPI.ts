669edafb2b7bc4d1194526a924c7065cd680bad2
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2024-07-23 01:52:45 +0000

    api base url

--- src/lib/api/core/OpenAPI.ts
+++ src/lib/api/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: '',
+    BASE: 'https://devblog-api.vercel.app',
     VERSION: '1.0.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
